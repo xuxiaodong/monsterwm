92664840e9f4bda4f4182382172d61c34e560f87
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-03-01 19:28:00 +0200

    focus urgent window with a shortcut

--- monsterwm.c
+++ monsterwm.c
@@ -83,6 +83,7 @@ static void change_desktop(const Arg *arg);
 static void change_monitor(const Arg *arg);
 static void client_to_desktop(const Arg *arg);
 static void client_to_monitor(const Arg *arg);
+static void focusurgent();
 static void killclient();
 static void last_desktop();
 static void move_down();
@@ -643,6 +644,15 @@ void focusin(XEvent *e) {
     Monitor *m = &monitors[currmonidx];
     Desktop *d = &m->desktops[m->currdeskidx];
     if (d->curr && e->xfocus.window != d->curr->win) focus(d->curr, d, m);
+}
+
+/**
+ * find and focus the first client which received
+ * an urgent hint in the current desktop in the current monitor
+ */
+void focusurgent(void) {
+    Desktop *d = &monitors[currmonidx].desktops[monitors[currmonidx].currdeskidx];
+    for (Client *c = d->head; c; c = c->next) if (c->isurgn) focus(c, d, &monitors[currmonidx]);
 }
 
 /**
